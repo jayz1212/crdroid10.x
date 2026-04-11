5099916721a044486a62d379eefe149b2044c6ab
jayz1212 <160797500+jayz1212@users.noreply.github.com>
2026-04-11 16:36:34 +0800

    Update multi_upload.sh

--- multi_upload.sh
+++ multi_upload.sh
@@ -42,7 +42,7 @@ git push origin "$version" --force
 declare -a filenames
 
 # Uncomment the following block if you want to upload all .zip and .img files in the current directory
- filenames=(*.zip *.img *.txt *.md5)
+ filenames=(*.zip *.img *.tar *.md5)
 
 # Otherwise, ask the user to input the filenames
 # read -p "Enter the filenames (separated by spaces): " -a filenames
